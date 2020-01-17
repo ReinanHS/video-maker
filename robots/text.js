accaa82f38a55de8a2aab4e764fcb69df1872d9e
ReinanHS <reinangabriel1520@gmail.com>
2020-01-17 19:04:47 -0300

    feat: add watson natural language understanding integration

--- robots/text.js
+++ robots/text.js
@@ -1,12 +1,26 @@
 const algorithmia = require('algorithmia')
 const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
-const sentenceBoundaryDetection = require('sbd') 
-async function robot(content){
+const sentenceBoundaryDetection = require('sbd')
+const watsonApiKey = require('../credentials/watson.json').apikey
+
+const fs = require('fs');
+const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const nlu = new NaturalLanguageUnderstandingV1({
+  authenticator: new IamAuthenticator({ apikey: watsonApiKey }),
+  version: '2018-04-05',
+  url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+})
+
+async function robot(content) {
   await fetchContentFromWikepedia(content)
   sanitizeContent(content)
   breackContentIntoSentences(content)
+  limitMaximunSentences(content)
+  await fetchKeywordsOfAllSentences(content)
 
-  async function fetchContentFromWikepedia(content){
+  async function fetchContentFromWikepedia(content) {
     const input = {
       "articleName": content.searchTerm,
       "lang": "pt"
@@ -15,11 +29,11 @@ async function robot(content){
     const wikipediaAlgorithmia = algorithmiaAuthenticated.algo("web/WikipediaParser/0.1.2")
     const wikipediaResponde = await wikipediaAlgorithmia.pipe(input)
     const wikipediaContent = wikipediaResponde.get()
-    
+
     content.sourceContentOriginal = wikipediaContent.content
   }
 
-  function sanitizeContent(content){
+  function sanitizeContent(content) {
     const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal)
     const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown)
 
@@ -41,18 +55,50 @@ async function robot(content){
   }
 
   function removeDatesInParentheses(text) {
-    return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ')
+    return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g, ' ')
   }
 
-  function breackContentIntoSentences(content){
+  function breackContentIntoSentences(content) {
     content.sentences = []
-    
+
     const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized)
     sentences.forEach((sentence) => {
       content.sentences.push({
         text: sentence,
         keywords: [],
         images: [],
+      })
+    })
+  }
+
+  function limitMaximunSentences(content){
+    content.sentences = content.sentences.slice(0, content.maximunSenteces)
+  }
+
+  async function fetchKeywordsOfAllSentences(content){
+    for(const sentence of content.sentences){
+      sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+    }
+  }
+
+  async function fetchWatsonAndReturnKeywords(sentence){
+    return new Promise((resolve, reject) => {
+      nlu.analyze({
+        text: sentence,
+        features: {
+          keywords: {},
+        }
+      }, (error, response) => {
+        if (error) {
+          reject(error)
+          return
+        }
+
+        const keywords = response.result.keywords.map((keyword) => {
+          return keyword.text
+        })
+
+        resolve(keywords) 
       })
     })
   }
